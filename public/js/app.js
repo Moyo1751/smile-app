5890bc213fe99187c408b3321202231044a62f58
LAPTOP-INCD9FJQ\Moyo <moyoboy15@gmail.com>
2022-12-08 21:29:00 +0000

    removed commented code in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,12 @@
+
 let apiRes = { question: '', solution: '' };
 const apiUrl = 'https://marcconrad.com/uob/smile/api.php?out=json';
-console.log('it is working');
 let score = 0;
 $(document).ready(function() {
     $('#next-btn').click(function (e) {
         getQuestionFromApi();
     })
     $('.answer-btns').click(function (e) {
-        //alert('answer button clicked!');
         console.log(e.target.value);
         const selectedAnswer = parseInt(e.target.value);
         checkAnswer(selectedAnswer)
@@ -20,7 +19,6 @@ function getQuestionFromApi() {
         url: apiUrl,
         success: function (result) {
             apiRes = result;
-            // console.log(apiRes);
             $('#question-img').attr('src', apiRes.question);
         },
 
